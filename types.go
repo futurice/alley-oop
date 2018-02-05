de1d1b263531270df3f70e7b550e911c4684ca50
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2018-02-05 20:40:35 +0200

    Add a Database interface for storing both DNS and certificate data.

--- types.go
+++ types.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"context"
+	"net"
+)
+
+type Database interface {
+	GetIPAddresses(ctx context.Context, fqdn string) ([]net.IP, error)
+	PutIPAddresses(ctx context.Context, fqdn string, addresses []net.IP) error
+	DeleteIPAddresses(ctx context.Context, fqdn string) error
+
+	GetTXTValues(ctx context.Context, fqdn string) ([]string, error)
+	PutTXTValues(ctx context.Context, fqdn string, values []string) error
+	DeleteTXTValues(ctx context.Context, fqdn string) error
+
+	GetCertificate(ctx context.Context, fqdn string) ([]byte, error)
+	PutCertificate(ctx context.Context, fqdn string, data []byte) error
+	DeleteCertificate(ctx context.Context, fqdn string) error
+}
+
 type AlleyOopConfig struct {
 	DNS dnsConfig
 }

6f8bc174e1840c7b9d389d0fb2ccde750455c99c
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2018-02-06 13:36:51 +0200

    Add proper locking to the MemoryDatabase.

--- memdb.go
+++ memdb.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"net"
+	"sync"
 )
 
 type MemoryDatabase struct {
+	sync.RWMutex
 	ipaddrs  map[string][]net.IP
 	txtvals  map[string][]string
 	certdata map[string][]byte
@@ -24,10 +26,14 @@ func (db *MemoryDatabase) DoesDomainExist(ctx context.Context, domain string) (b
 }
 
 func (db *MemoryDatabase) GetIPAddresses(ctx context.Context, domain string) ([]net.IP, error) {
+	db.RLock()
+	defer db.RUnlock()
 	return db.ipaddrs[domain], nil
 }
 
 func (db *MemoryDatabase) PutIPAddresses(ctx context.Context, domain string, addresses []net.IP) error {
+	db.Lock()
+	defer db.Unlock()
 	if db.ipaddrs == nil {
 		db.ipaddrs = make(map[string][]net.IP)
 	}
@@ -36,15 +42,21 @@ func (db *MemoryDatabase) PutIPAddresses(ctx context.Context, domain string, add
 }
 
 func (db *MemoryDatabase) DeleteIPAddresses(ctx context.Context, domain string) error {
+	db.Lock()
+	defer db.Unlock()
 	delete(db.ipaddrs, domain)
 	return nil
 }
 
 func (db *MemoryDatabase) GetTXTValues(ctx context.Context, domain string) ([]string, error) {
+	db.RLock()
+	defer db.RUnlock()
 	return db.txtvals[domain], nil
 }
 
 func (db *MemoryDatabase) PutTXTValues(ctx context.Context, domain string, values []string) error {
+	db.Lock()
+	defer db.Unlock()
 	if db.txtvals == nil {
 		db.txtvals = make(map[string][]string)
 	}
@@ -53,15 +65,21 @@ func (db *MemoryDatabase) PutTXTValues(ctx context.Context, domain string, value
 }
 
 func (db *MemoryDatabase) DeleteTXTValues(ctx context.Context, domain string) error {
+	db.Lock()
+	defer db.Unlock()
 	delete(db.txtvals, domain)
 	return nil
 }
 
 func (db *MemoryDatabase) GetCertificate(ctx context.Context, domain string) ([]byte, error) {
+	db.RLock()
+	defer db.RUnlock()
 	return db.certdata[domain], nil
 }
 
 func (db *MemoryDatabase) PutCertificate(ctx context.Context, domain string, data []byte) error {
+	db.Lock()
+	defer db.Unlock()
 	if db.certdata == nil {
 		db.certdata = make(map[string][]byte)
 	}
@@ -70,6 +88,8 @@ func (db *MemoryDatabase) PutCertificate(ctx context.Context, domain string, dat
 }
 
 func (db *MemoryDatabase) DeleteCertificate(ctx context.Context, domain string) error {
+	db.Lock()
+	defer db.Unlock()
 	delete(db.certdata, domain)
 	return nil
 }
